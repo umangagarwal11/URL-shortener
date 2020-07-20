fd98bf5a5b98221a776409c0df623da8179e65ae
Umang Agarwal <46571828+umangagarwal11@users.noreply.github.com>
2020-07-20 23:45:49 +0530

    try

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ app.get('/admin',async (req,res) =>{
   res.render('index',{shortUrls:shortUrls});
 });
 
-app.post('/admin',async (req,res) => {
-  await ShortUrls.create({full:req.body.fullUrl, short:req.body.shortUrl});
+app.post('/admin',(req,res) => {
+  ShortUrls.create({full:req.body.fullUrl, short:req.body.shortUrl}).then(()=>{
   const shortUrls=await ShortUrls.find();
   res.render('index',{shortUrls:shortUrls});
+});
 });
 
 app.get('/:short', async (req,res) => {
